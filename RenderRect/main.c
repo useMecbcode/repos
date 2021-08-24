dffb146f28f72281f4b36082d9c2372b20080b11
kali <lee.wyt.lly@gmail.com>
2021-08-24 01:50:26 -0400

    Fixed

--- RenderRect/main.c
+++ RenderRect/main.c
@@ -5,9 +5,12 @@
 const int WIDTH = 400;
 const int HEIGHT = 300;
 const int FrameRate = 30;
-int x = 0;
-void event_loop(SDL_Surface * screen, SDL_Window*win);
-void draw(SDL_Surface * screen, SDL_Window*win);
+
+
+SDL_Renderer *renderer;
+SDL_Window *win;
+void event_loop();
+void draw();
 int main() {
     if(SDL_Init(SDL_INIT_VIDEO))
     {
@@ -15,7 +18,7 @@ int main() {
         return 1;
     }
     //CREATE WINDOW
-    SDL_Window *win = SDL_CreateWindow(
+    win = SDL_CreateWindow(
             "Hello World",SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED,
             WIDTH,HEIGHT,
@@ -26,32 +29,37 @@ int main() {
         SDL_Log("Can not Create Window, %s",SDL_GetError());
         return 1;
     }
+    renderer = SDL_CreateRenderer(win,-1,SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL)
+    {
+        SDL_Log("Can't create renderer %s",SDL_GetError());
+    }
     SDL_Surface *screen = SDL_GetWindowSurface(win);
-
-    event_loop(screen,win);
+    event_loop();
     //Destroy WINDOW
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(win);
     SDL_FreeSurface(screen);
     return 0;
 }
-void draw(SDL_Surface * screen, SDL_Window*win)
+void draw()
 {
-    x++;
-    //memory data express pixel value from window,is renderer
-    SDL_Rect rect = {0,0,WIDTH,HEIGHT};
-    //color ARGB
-    SDL_FillRect(screen,&rect,0xffffffff);
-    // Draw squares
-    SDL_Rect RedRect = {x,0,100,100};
-    SDL_FillRect(screen,&RedRect,0xffff0000);
-    SDL_UpdateWindowSurface(win);
+    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+    SDL_RenderClear(renderer);
+
+    SDL_Rect r = {0,0,100,100};
+    SDL_SetRenderDrawColor(renderer,255,0,0,100);
+    SDL_SetRenderDrawBlendMode(renderer,SDL_BLENDMODE_BLEND);
+    SDL_RenderFillRect(renderer,&r);
+
+    SDL_RenderPresent(renderer);
 }
-void event_loop(SDL_Surface * screen, SDL_Window*win)
+void event_loop()
 {
     while (true)
     {
         uint32_t start = SDL_GetTicks();
-        draw(screen,win);
+        draw();
         SDL_Event event;
         SDL_PollEvent(&event);
         while(SDL_PollEvent(&event)){
